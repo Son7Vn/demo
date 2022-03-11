20b9c6595a5a747bad98175e2672956ca628960f
Son7Vn <sonpt0707@gmail.com>
2022-03-11 16:38:52 +0700

    yu
    u

---
+++ AuthenticationApp/AuthenticationApp/AuthenticationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationAppApp.swift
+//  AuthenticationApp
+//
+//  Created by SonGoku on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
